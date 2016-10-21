d32c74f467e036757104e679baee2433b365a943
Kenny Williams <kennyjwilli@gmail.com>
2016-10-21 15:27:54 -0700

    Add support for clojure.spec.test/check

--- build.boot
+++ build.boot
@@ -1,11 +1,12 @@
 (def project 'provisdom/test)
-(def version "0.2.1")
+(def version "0.2.2-SNAPSHOT")
 
 (set-env! :resource-paths #{"src"}
           :source-paths #{"test"}
           :dependencies '[[provisdom/boot-tasks "0.7.0" :scope "test"]
                           [adzerk/boot-test "1.1.1" :scope "test"]
                           [incanter "1.5.7" :scope "test"]
+                          [org.clojure/test.check "0.9.0" :scope "test"]
                           [org.clojure/clojure "1.8.0" :scope "provided"]])
 
 (task-options!
@@ -18,4 +19,4 @@
                      "http://www.eclipse.org/legal/epl-v10.html"}})
 
 (require '[adzerk.boot-test :refer [test]]
-         '[provisdom.boot-tasks.core :refer [build push-jar]])
+         '[provisdom.boot-tasks.core :refer [build auto-build push-jar]])
--- src/provisdom/test/core.clj
+++ src/provisdom/test/core.clj
@@ -1,13 +1,15 @@
 (ns provisdom.test.core
-  (:require [clojure.test :refer [deftest is are try-expr assert-expr do-report]]))
+  (:require [clojure.test :as t]
+            [clojure.pprint :as pprint]
+            [clojure.string :as str]))
 
 (defmacro is=
   [expected actual]
-  `(is (= ~expected ~actual)))
+  `(t/is (= ~expected ~actual)))
 
 (defmacro is-not
   ([form] `(is-not ~form nil))
-  ([form msg] `(is (not ~form) ~msg)))
+  ([form msg] `(t/is (not ~form) ~msg)))
 
 (defn midje-just
   [expected actual]
@@ -19,18 +21,42 @@
                      (= e a))) expected actual))
     false))
 
-(defmethod assert-expr 'just
+(defmethod t/assert-expr 'just
   [msg form]
   `(let [expected# ~(nth form 1)
          actual# ~(nth form 2)
          result# (midje-just expected# actual#)]
      (if result#
-       (do-report {:type     :pass
-                   :message  ~msg
-                   :expected '~form
-                   :actual   actual#})
-       (do-report {:type     :fail
-                   :message  ~msg
-                   :expected '~(nth form 1)
-                   :actual   actual#}))
+       (t/do-report {:type     :pass
+                     :message  ~msg
+                     :expected '~form
+                     :actual   actual#})
+       (t/do-report {:type     :fail
+                     :message  ~msg
+                     :expected '~(nth form 1)
+                     :actual   actual#}))
      result#))
+
+(defmacro defspec-test
+  ([name sym-or-syms] `(defspec-test ~name ~sym-or-syms nil))
+  ([name sym-or-syms opts]
+   (when t/*load-tests*
+     `(def ~(vary-meta name assoc :test `(fn []
+                                           (let [check-results# (clojure.spec.test/check ~sym-or-syms ~opts)
+                                                 checks-passed?# (every? nil? (map :failure check-results#))]
+                                             (if checks-passed?#
+                                               (t/do-report {:type    :pass
+                                                             :message (str "Generative tests pass for "
+                                                                           (str/join ", " (map :sym check-results#)))})
+                                               (doseq [failed-check# (filter :failure check-results#)
+                                                       :let [r# (clojure.spec.test/abbrev-result failed-check#)
+                                                             failure# (:failure r#)]]
+                                                 (t/do-report
+                                                   {:type     :fail
+                                                    :message  (with-out-str (clojure.spec/explain-out failure#))
+                                                    :expected (->> r# :spec rest (apply hash-map) :ret)
+                                                    :actual   (if (instance? Throwable failure#)
+                                                                failure#
+                                                                (:clojure.spec.test/val failure#))})))
+                                             checks-passed?#)))
+        (fn [] (t/test-var (var ~name)))))))
